9d8d6f1ecc98233c28dc9a22456489ca746ef0dc
ApoloIslaio <apoloislaio@gmail.com>
2023-07-18 16:07:19 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,94 @@
+import { useEffect, useRef, useState } from 'react'
+import './App.css'
+//import Camera from 'react-html5-camera-photo';
+//import 'react-html5-camera-photo/build/css/index.css';
+import Webcam from 'react-webcam';
+
+const App = () => {
+
+  const [ipAddress, setIPAddress] = useState('')
+  const [latitude, setLatitude] = useState(0)
+  const [longitude, setLongitude] = useState(0)
+  const [error, setError ] = useState('')
+  const [dataNow, setDataNow] = useState<Data>(0);
+  const [imgUrl, setImageUrl] = useState('')
+  const webcamRef = useRef(null);
+ 
+  
+  const geolocationAPI = navigator.geolocation;
+
+  //get ip
+  function getIpAddress(){
+    fetch('https://api.ipify.org?format=json')
+      .then(response => response.json())
+      .then(data => setIPAddress(data.ip))
+      .catch(error => console.log(error))
+  }
+
+  //latitude e longitude
+  const getUserCoordinates = () => {
+    if (!geolocationAPI) {
+      setError('Geolocation API is not available in your browser!')
+    } else {
+      geolocationAPI.getCurrentPosition((position) => {
+        const { coords } = position;
+        setLatitude(coords.latitude);
+        setLongitude(coords.longitude);
+      }, (error) => {
+        setError('Something went wrong getting your position!', error)
+      })
+    }
+  }
+
+  //get camera user 
+  const capturePhoto = () => {
+    const imgSrc = webcamRef.current.getScreenshot()
+    console.log('Imagem capturada:', imgSrc);
+    setImageUrl(imgSrc)
+   }
+
+  // get files user
+  // get contact user
+
+  function handleClickBtn(){
+    getIpAddress()   
+    getUserCoordinates()
+    capturePhoto()
+    const timeElapsed = Date.now();
+    const today = new Date(timeElapsed);
+    const data = today.toLocaleString()
+    setDataNow(data)
+    console.log('CLick')
+  }
+
+
+  useEffect(() => {
+     
+     
+  }, []);
+
+  return (
+    <>
+      <div> 
+        <p>Ip: {ipAddress}</p>
+        <p>Localização: {latitude} {longitude}</p>
+        <p>Data: { dataNow }</p>
+        
+        <Webcam
+          audio={false}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          style={{width: 200, height: 200}}
+        />
+
+        <button type="button" onClick={handleClickBtn}>Clique aqui</button>
+
+        <img src={imgUrl} alt="imagem" style={{width: 200, height: 200}} />
+        
+      </div>
+        
+    </>
+  )
+}
+
+export default App
